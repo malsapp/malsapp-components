e462a0a83ed3034c00027cfb0569c6959906d0a2
Mohamed Shaban <m.shaban@malsapp.com>
2017-03-07 15:39:07 +0200

    Adding the inital version

---
+++ index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {
+  ActivityIndicator,
+  View,
+} from 'react-native';
+
+import Default from './templates/Default'
+
+function getProperty(object, string) {
+  if (!string) {
+    return null;
+  }
+
+  const arr = string.split('.');
+  let prop = object;
+
+  arr.forEach(key => (prop = prop && prop[key] && prop[key]));
+
+  return prop;
+}
+
+const Annotation = (property, LoadingIndicator = Default) => Child => (props) => {
+  const isLoading = getProperty(props, property);
+  if (isLoading) {
+    return (<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <ActivityIndicator />
+    </View>);
+  }
+
+  return (<Child />);
+};
+
+Annotation.DEFAULT = Default;
+
+export default Annotation
